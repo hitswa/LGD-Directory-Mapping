0f67fdea87a610a934cb5a6cffe9c3df464a545b
Hitesh Kumar <rohilla.hitesh@gmail.com>
2024-06-23 13:28:44 +0530

    refactor: :sparkles: implemented recommendations

--- MapLGD.py
+++ MapLGD.py
@@ -4,49 +4,49 @@ import json
 
 filePath = "Data/LGD Latest/Andaman And Nicobar Islands/"
 
-def checkEmptyRow(arrRow):
-    emptyStatus = False
+def check_empty_row(row):
+    empty_status = False
 
-    totalEmptyCell = 0
+    total_empty_cell = 0
 
-    for cell in arrRow:
+    for cell in row:
         if cell == "":
-            totalEmptyCell = totalEmptyCell + 1
+            total_empty_cell = total_empty_cell + 1
     
-    if totalEmptyCell == len(arrRow):
-        emptyStatus = True
+    if total_empty_cell == len(row):
+        empty_status = True
 
-    return emptyStatus
+    return empty_status
 
-def writeCsvFile(data, file):
+def write_csv_file(data, file):
 
-    csvData = []
+    csv_data = []
 
     for row in data:
-        csvData.append('"' + '","'.join(row) + '"')
+        csv_data.append('"' + '","'.join(row) + '"')
 
     with open(file + '.csv', 'w') as f:
-        f.write('\n'.join(csvData))
+        f.write('\n'.join(csv_data))
 
-def writeJsonFile(data, file):
+def write_json_file(data, file):
 
-    jsonData = []
+    json_data = []
 
     for row in data:
-        rowJson = {}
+        row_json = {}
         index = 0
         for cell in row:
             if index != 0:
-                rowJson |= {data[0][index]: cell}
+                row_json |= {data[0][index]: cell}
             index = index + 1
             
 
-        jsonData.append(rowJson)
+        json_data.append(row_json)
         
     with open(file + '.json', "w") as json_file:
-        json.dump(jsonData, json_file, indent=4)
+        json.dump(json_data, json_file, indent=4)
 
-def getCsvData( path, file, columns ):
+def get_csv_data( path, file, columns ):
     with open(path + file + '.xls', 'r') as f:
         xml_data = f.read()
 
@@ -54,43 +54,41 @@ def getCsvData( path, file, columns ):
 
     rows = dict_data["Workbook"]["Worksheet"]["ss:Table"]["Row"]
 
-    finalData = []
+    final_data = []
 
     for row in rows:
         if not isinstance(row, list):  # Check if "Row" is a list
             temp = []
-            if row.get("Cell"):
-                if isinstance(row.get("Cell"), list):
-                    for data in row.get("Cell"):
-                        if( data.get("Data") ):
-                            a = data.get("Data").get("#text")
-                            if a is None:
-                                temp.append("")
-                            else:
-                                temp.append(a.replace("  ", " ").replace("  ", " ").replace("  ", " ").replace("( ", "(").replace(" )", ")").replace("\n", " "))
-                        else:
+            if row.get("Cell") and isinstance(row.get("Cell"), list):
+                for data in row.get("Cell"):
+                    if( data.get("Data") ):
+                        a = data.get("Data").get("#text")
+                        if a is None:
                             temp.append("")
-            if( len(temp) == columns ):
-                if( checkEmptyRow(temp) == False):
-                    finalData.append(temp)
+                        else:
+                            temp.append(a.replace("  ", " ").replace("  ", " ").replace("  ", " ").replace("( ", "(").replace(" )", ")").replace("\n", " "))
+                    else:
+                        temp.append("")
+            if( (len(temp) == columns) and (check_empty_row(temp) == False) ):
+                final_data.append(temp)
 
-    writeCsvFile(finalData, file)
-    writeJsonFile(finalData, file)
+    write_csv_file(final_data, file)
+    write_json_file(final_data, file)
 
-    return finalData
+    return final_data
 
-allBlockStateWithCoveredVillage = getCsvData(filePath, "allBlockStateWithCoveredVillage12024:06:23:00:50:11:325", 8)
-blockofspecificState = getCsvData(filePath, "blockofspecificState2024:06:23:00:50:11:258", 7)
-districtofSpecificState = getCsvData(filePath, "districtofSpecificState2024:06:23:00:50:10:581", 7)
-priLbSpecificState = getCsvData(filePath, "priLbSpecificState2024:06:23:00:50:10:828", 8)
-priWards = getCsvData(filePath, "priWards2024:06:23:00:50:11:073", 10)
-subDistrictofSpecificState = getCsvData(filePath, "subDistrictofSpecificState2024:06:23:00:50:10:606", 9)
-tlbSpecificState = getCsvData(filePath, "tlbSpecificState2024:06:23:00:50:10:894", 8)
-ulbSpecificState = getCsvData(filePath, "ulbSpecificState2024:06:23:00:50:10:855", 9)
-uLBWardforState = getCsvData(filePath, "uLBWardforState2024:06:23:00:50:10:920", 6)
-uLBWardforStateWithCov = getCsvData(filePath, "uLBWardforStateWithCov2024:06:23:00:50:10:981", 10)
-villageGramPanchayatMapping = getCsvData(filePath, "villageGramPanchayatMapping2024:06:23:00:50:11:226", 15)
-villageofSpecificState = getCsvData(filePath, "villageofSpecificState2024:06:23:00:50:10:782", 13)
+all_block_state_with_covered_village = get_csv_data(filePath, "allBlockStateWithCoveredVillage12024:06:23:00:50:11:325", 8)
+block_of_specific_state = get_csv_data(filePath, "blockofspecificState2024:06:23:00:50:11:258", 7)
+district_of_specific_state = get_csv_data(filePath, "districtofSpecificState2024:06:23:00:50:10:581", 7)
+priLb_specific_state = get_csv_data(filePath, "priLbSpecificState2024:06:23:00:50:10:828", 8)
+pri_wards = get_csv_data(filePath, "priWards2024:06:23:00:50:11:073", 10)
+sub_district_of_specific_state = get_csv_data(filePath, "subDistrictofSpecificState2024:06:23:00:50:10:606", 9)
+tlb_specific_state = get_csv_data(filePath, "tlbSpecificState2024:06:23:00:50:10:894", 8)
+ulb_specific_state = get_csv_data(filePath, "ulbSpecificState2024:06:23:00:50:10:855", 9)
+ulb_ward_for_state = get_csv_data(filePath, "uLBWardforState2024:06:23:00:50:10:920", 6)
+ulb_ward_for_state_with_cov = get_csv_data(filePath, "uLBWardforStateWithCov2024:06:23:00:50:10:981", 10)
+village_gram_panchayat_mapping = get_csv_data(filePath, "villageGramPanchayatMapping2024:06:23:00:50:11:226", 15)
+village_of_specific_state = get_csv_data(filePath, "villageofSpecificState2024:06:23:00:50:10:782", 13)
 
 
 
